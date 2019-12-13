71101157bc110639feac8f6f7bfeca7782a33071
bhaga <bhaga@DESKTOP-KEBG4LU.fios-router.home>
2019-12-13 10:29:29 -0500

    This is my first practice on Github project. i complete account automation script.

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module Githubmadhavipractice {
+}
---
+++ src/myfirstpractice/Batch236madhavipractice.java
@@ -0,0 +1,10 @@
+package myfirstpractice;
+
+public class Batch236madhavipractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
